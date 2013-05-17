da1107d2cdf375bcd5dc0ed448dff5be170b52cc
Brandon Jurewicz <brandonjur@gmail.com>
2013-05-17 15:10:53 -0400

    Nearly fully working custom user called Juser

--- articles/urls.py
+++ articles/urls.py
@@ -1,3 +1,7 @@
-from articles.models import Article
-
-urlpatterns = Article.get_urls()
+from articles.views import ArticleList, ArticleDetail
+from django.conf.urls.defaults import patterns, url
+#urlpatterns = Article.get_urls()
+urlpatterns = patterns('',
+            url(r'^$', ArticleList.as_view(), name='article_index'),
+            url(r'^(?P<slug>[a-z0-9_-]+)/$', ArticleDetail.as_view(), name='article_detail'),
+        )
